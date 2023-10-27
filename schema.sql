aee333062f197a1de8606ec941768dff16fe59fa
Goldinium <badrbenslama8@gmail.com>
2023-10-27 18:30:07 +0100

    SQL joining tables

--- schema.sql
+++ schema.sql
@@ -11,3 +11,25 @@ weight_kg DECIMAL(4,2));
 /*Add a column species of type string to your animals table*/
 
 ALTER TABLE animals ADD COLUMN species VARCHAR(50);
+
+/*Add a table for owners*/
+
+CREATE TABLE owners (id SERIAL PRIMARY KEY, full_name VARCHAR(50), age INTEGER);
+
+/*Add a table for pokemons species*/
+
+CREATE TABLE species (id SERIAL PRIMARY KEY, name VARCHAR(50));
+
+/*animals table ID is already an autoincremented PRIMARY KEY*/
+
+/*remove species colomun*/
+
+ALTER TABLE animals DROP COLUMN species;
+
+/*Add column species_id which is a foreign key referencing species table*/
+
+ALTER TABLE animals ADD COLUMN species_id integer REFERENCES species(id);
+
+/*Add column owner_id which is a foreign key referencing the owners table*/
+
+ALTER TABLE animals ADD COLUMN owner_id integer REFERENCES owners(id);
